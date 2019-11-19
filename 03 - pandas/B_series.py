88339998840fc1e12094e180c5a007722fcd62b3
KevinMendoza <patricio.mendoza@epn.edu.ec>
2019-11-19 09:00:35 -0500

    Pandas

---
+++ 03 - pandas/B_series.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 19 08:00:10 2019
+
+@author: kevme
+"""
+
+import numpy as np
+import pandas as pd
+
+lista_numeros = [1,2,3,4]
+tupla_numeros = (1,2,3,4)
+np_numeros = np.array((1,2,3,4))
