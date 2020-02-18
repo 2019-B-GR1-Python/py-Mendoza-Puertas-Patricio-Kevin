a79f5058f3be7670c52c556379adb77bb8069cb8
KevinMendoza <patricio.mendoza@epn.edu.ec>
2020-02-18 11:48:25 -0500

    Deber

--- 04 - Scrapy/06-spider-items/arania_fybeca/arania_fybeca/spiders/analisis.py
+++ 04 - Scrapy/06-spider-items/arania_fybeca/arania_fybeca/spiders/analisis.py
@@ -1,26 +1,35 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import os
-import nltk
-import seaborn as sns
 
-#path del csv creado
-path_csv = "C://Users//kevme//Documents//GitHub//py-Mendoza-Puertas-Patricio-Kevin//04 - Scrapy//06-spider-items//arania_fybeca//arania_fybeca//spiders//tmp//productos-fybeca.csv"
-df = pd.read_csv(path_csv,  encoding = 'unicode_escape',sep = ",")
 
-# Ahorro por la compre con tarjeta fybeca
-df['ahorro'] = df['precio_normal'] - df['precio_descuento']
+###El archivo csv se encuentra en la siguiente direccion, este se lo guardara
+###en el siguiente path.
+path = "C://Users//kevme//Documents//GitHub//py-Mendoza-Puertas-Patricio-Kevin//04 - Scrapy//06-spider-items//arania_fybeca//arania_fybeca//spiders//tmp//productos-fybeca.csv"
 
-ahorroCompra = df['ahorro'].sum()
-descuentoTotal = df['ahorro'].max()
-descuetoMinimo = df['ahorro'].min()
+###Al dataframe de prueba se lo usara de la siguiente manera.
+df_prueba = pd.read_csv(path,  encoding = 'unicode_escape',sep = ",")
 
-#ver calculo descuento Total
-print("Precio con el descuento maximo : ",descuentoTotal)
+###Para continuar se guardara en un pickle, con la siguiente configuracion.
+path_guardado_pickle = "C://Users//kevme//Documents//GitHub//py-Mendoza-Puertas-Patricio-Kevin//04 - Scrapy//06-spider-items//arania_fybeca//arania_fybeca//spiders//tmp//a.pickle"
 
-#ver calculo descuento minimo
-print('Precio con el descuento minimo : ', descuetoMinimo)
+df_prueba.to_pickle(path_guardado_pickle)
 
-#Ahorro por la compra con tareta
-print('Ahorro por compra con tarjeta : ', ahorroCompra)
+df_pickle = pd.read_pickle(path_guardado_pickle)
+
+###El archivo original se tienen mas de 15000 jugadores, para este proyecto se usaran
+###solo los primeros 100. 
+df = df_pickle.iloc[1:200,:].copy()
+
+###y para finalizar y poder realizar el analisis se creara el xlsx del proyecto.
+df.to_excel('C://Users//kevme//Documents//GitHub//py-Mendoza-Puertas-Patricio-Kevin//04 - Scrapy//06-spider-items//arania_fybeca//arania_fybeca//spiders//tmp//mi_proyecto.xlsx')
+
+###Para poder ver lo que contiene nuestro dataframe y poder continuar con el
+###analiis, usaremos la siguiente linea.
+df.head()
+
+df
+
+
+#compra con tarjeta y sin tarjeta
+df.loc[df.index[-1] + 1] = df.sum()
+print (df)
+
